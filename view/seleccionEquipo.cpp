29492bdbef594a3ee06d5e7967fd04523ab3b177
CesarLandaverdeuca <00057426@uca.edu.sv>
2026-06-23 08:52:11 -0600

    avance la funcion de guardar pokemon, me flata que tenga la opcion que borre un espacio del ciclo si detecta repetido y guardar el pokemon en .txt

--- view/seleccionEquipo.cpp
+++ view/seleccionEquipo.cpp
@@ -6,6 +6,7 @@ Comienzo para plantear las bases de la selleccion de equipo
 #include <string>
 #include <ctime>
 #include "../model/pokedex.cpp"
+
 using namespace std;
 
 void MostrarPokemones()
@@ -24,4 +25,51 @@ void MostrarPokemones()
 void GuardarEquipoPokemon()
 {
     cout << "Bienvenido es el turno de elegir tu equipo pokemon(solo puedes elegir 6 max)" << endl;
+    MostrarPokemones();
+    Equipo temp;
+    for (int i = 0; i < 6; i++)
+    { 
+          int seleccionUser;
+          cout<<"Elige tu pokemon segun su numero"<<i+1<<endl;
+        cin>>seleccionUser;
+
+        
+         while (seleccionUser>30  || seleccionUser<=0)
+        {
+            cout<<"Te has equivocado ingresa de nuevo el numero de pokemon a elegir"<<endl;
+             cout<<"Elige tu pokemon segun su numero"<<i+1<<endl;
+              cin>>seleccionUser;
+
+        }
+            int indiceReal = seleccionUser-1;
+
+        bool pokeRep=false;
+
+        for (int j = 0; j!=i; j++)
+        {
+           if (pokedexDisponible[indiceReal].nombre == temp.pokemon[j].nombre)
+           {
+            cout<<"Pokemon repetido"<<endl;
+             pokeRep =true;
+           }
+           
+        }
+         if (pokeRep==true)
+         {
+           temp.pokemon[i] = pokedexDisponible[indiceReal];
+         }
+         
+        
+
+        cout << "¡Has agregado a " << temp.pokemon[i].nombre << " a tu equipo!" << endl;
+
+        
+        
+        
+      
+
+
+        
+    }
+    
 }
